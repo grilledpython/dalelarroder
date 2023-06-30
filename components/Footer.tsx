57d4895eb9d9dc77fbaa3fe8954dfa3933e4e213
Arpan M <121425759+grilledpython@users.noreply.github.com>
2023-06-30 22:05:09 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
             <ul className="flex cursor-pointer items-center space-x-5">
               <li>
                 <a
-                  href={siteMetadata.linkedin}
+                  href="https://apms.cf"
                   target="_blank"
                   rel="noreferrer"
                   aria-label="linkedin"
