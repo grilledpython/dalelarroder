c742000608092fd8d6d0a14f79a7278840e3a2cf
Arpan M <121425759+grilledpython@users.noreply.github.com>
2023-06-30 21:48:38 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import MainLayout from '@/layouts/MainLayout';
 import { allAuthors } from 'contentlayer/generated';
 
 export const metadata = {
-  title: 'About - Dale Larroder',
-  description: 'About me - Dale Larroder',
+  title: 'About - Arpan Mandal',
+  description: 'About me - Arpan Mandal',
 };
 
 export default function About() {
