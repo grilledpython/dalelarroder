bed6756de8f49814ea6d0396b0b082b4804204d2
Arpan M <121425759+grilledpython@users.noreply.github.com>
2023-06-30 21:56:07 +0530

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,8 +2,8 @@ import Projects from '@/components/Projects/Projects';
 import MainLayout from '@/layouts/MainLayout';
 
 export const metadata = {
-  title: 'Projects - Dale Larroder',
-  description: 'My Projects - Dale Larroder',
+  title: 'Projects - Arpan Mandal',
+  description: 'My Projects - Arpan Mandal',
 };
 
 export default function Page() {
