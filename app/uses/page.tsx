de28a6d23fae93e43158176a8c71f973fad22723
Arpan M <121425759+grilledpython@users.noreply.github.com>
2023-06-30 21:55:22 +0530

    Update page.tsx

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -4,8 +4,8 @@ import MainLayout from '@/layouts/MainLayout';
 import { allAuthors } from 'contentlayer/generated';
 
 export const metadata = {
-  title: 'Uses - Dale Larroder',
-  description: 'What I Use - Dale Larroder',
+  title: 'Uses - Arpan Mandal',
+  description: 'What I Use - Arpan Mandal',
 };
 
 export default function Uses() {
