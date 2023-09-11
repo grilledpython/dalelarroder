84f1cc1d56694620bfc31ee1a33beacb128d2f86
Never To Be Known <121425759+grilledpython@users.noreply.github.com>
2023-09-11 13:26:50 +0530

    Update constants.ts

--- components/Projects/constants.ts
+++ components/Projects/constants.ts
@@ -3,7 +3,7 @@ import { Project } from './types';
 export const projects: Project[] = [
   {
     title: 'Diino Marketplace',
-    src: 'field-app.webp',
+    src: 'w1.webp',
     color: '#dbeafe',
     url: 'https://app.aphex.co/',
     role: 'Fullstack Developer',
